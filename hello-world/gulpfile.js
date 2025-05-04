03f541080cbd736e2a873913d9a1a736c191b9dc
akhanalcs <ashishkhanalcs@gmail.com>
2025-05-03 20:23:48 -0400

    Gulp notes

--- hello-world/gulpfile.js
+++ hello-world/gulpfile.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// This runs 'hello-world/node_modules/@microsoft/sp-build-web/lib-commonjs/index.js' file
 const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
 var getTasks = build.rig.getTasks;
+
+// It overrides the getTasks function to use serve-deprecated instead of serve
 build.rig.getTasks = function () {
   var result = getTasks.call(build.rig);
 
